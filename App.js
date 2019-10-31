f9b369179f5b9bd66e89ca85185b973c08f620b6
Daniel Boehmke <daniel@webappclouds.com>
2019-10-31 13:47:38 -0400

    Update app.js

--- App.js
+++ App.js
@@ -33,11 +33,48 @@ export default class barcodeScanner extends Component {
         this.handleTourch(this.state.torchOn)}>
           <Image style = {styles.cameraIcon}
           source={this.state.torchOn === true ?
-          require('../../images/flasher_on.png') :
-        require('../../images/flasher_off.png')}/>
+          require('./images/flasher_on.png') :
+          require('./images/flasher_off.png')}/>
         </TouchableOpacity>
       </View>
      </View>
    )
  }
+
+ onBarCodeRead = (e) => {
+   Alert.alert("Barcode value is " + e.data ," Barcode type is " + e.type);
+ }
+
+ handleTourch(value) {
+   if (value === true){
+     this.setState({ torchOn: false});
+   } else {
+     this.setState({ torchOn: true});
+   }
+ }
 }
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     flexDirection: 'row'
+   },
+
+   preview: {
+     flex: 1,
+     justifyContent: 'flex-end',
+     alignItems: 'center'
+   },
+   cameraIcon: {
+     margin: 5,
+     height: 40,
+     width: 40
+   },
+
+   bottomOverlay: {
+     position: "absolute",
+     width: "100%",
+     flex: 20,
+     flexDirection: "row",
+     justifyContent: 'space-between'
+   },
+ });
