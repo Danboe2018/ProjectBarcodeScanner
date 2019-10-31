5c541e5233a8a69543d80a36a6e3bea2fcb1b5a7
Daniel Boehmke <daniel@webappclouds.com>
2019-10-31 12:40:27 -0400

    Adds render method

--- App.js
+++ App.js
@@ -14,4 +14,30 @@ export default class barcodeScanner extends Component {
       torchOn: false
     }
  }
+ render(){
+   return (
+     <View style ={styles.container}>
+       <Camera
+       style = {styles.preview}
+       torchMode = {this.state.torchOn ? Camera.constants.torchMode.on:
+      Camera.constants.torchMode.off}
+      onBarCodeRead = {this.onBarCodeRead}
+      ref={cam => this.camera = cam}
+      aspect={Camera.constants.aspect.fill}>
+        <Text style={{
+          backgroundColor:'white'
+        }}>BARCODE SCANNER</Text>
+      </Camera>
+      <View style = {styles.bottomOverlay}>
+        <TouchableOpacity onPress={() =>
+        this.handleTourch(this.state.torchOn)}>
+          <Image style = {styles.cameraIcon}
+          source={this.state.torchOn === true ?
+          require('../../images/flasher_on.png') :
+        require('../../images/flasher_off.png')}/>
+        </TouchableOpacity>
+      </View>
+     </View>
+   )
+ }
 }
