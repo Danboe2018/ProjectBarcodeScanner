7810278a24e013fb159aabbe8d923bd81c971fab
Dan Boehmke <Daniel@webappclouds.com>
2019-10-31 15:11:10 -0400

    Changed camera used

--- App.js
+++ App.js
@@ -1,80 +1,90 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Text,
   View,
   StyleSheet, Alert, TouchableOpacity,
   Image
 } from 'react-native';
-import Camera from 'react-native-camera'
+import { RNCamera, FaceDetector } from 'react-native-camera';
 export default class barcodeScanner extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.handTourch = this.handleTourch.bind(this);
     this.state = {
       torchOn: false
     }
- }
- render(){
-   return (
-     <View style ={styles.container}>
-       <Camera
-       style = {styles.preview}
-       torchMode = {this.state.torchOn ? Camera.constants.TorchMode.on :
-       Camera.constants.TorchMode.off}
-        onBarCodeRead = {this.onBarCodeRead}
-        ref={cam => this.camera = cam}
-        aspect={Camera.constants.aspect.fill}>
-        <Text style={{
-          backgroundColor:'white'
-        }}>BARCODE SCANNER</Text>
-      </Camera>
-      <View style = {styles.bottomOverlay}>
-        <TouchableOpacity onPress={() =>
-        this.handleTourch(this.state.torchOn)}>
-          <Image style = {styles.cameraIcon}
-          source={this.state.torchOn === true ?
-          require('./images/flasher_on.png') :
-          require('./images/flasher_off.png')}/>
-        </TouchableOpacity>
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+        >
+          <Text style={{
+            backgroundColor: 'white'
+          }}>BARCODE SCANNER</Text>
+        </RNCamera>
+        <View style={styles.bottomOverlay}>
+          <TouchableOpacity onPress={() =>
+            this.handleTourch(this.state.torchOn)}>
+            <Image style={styles.cameraIcon}
+              source={this.state.torchOn === true ?
+                require('./images/flasher_on.png') :
+                require('./images/flasher_off.png')} />
+          </TouchableOpacity>
+        </View>
       </View>
-     </View>
-   )
- }
+    )
+  }
 
- onBarCodeRead = (e) => {
-   Alert.alert("Barcode value is " + e.data ," Barcode type is " + e.type);
- }
+  onBarCodeRead = (e) => {
+    Alert.alert("Barcode value is " + e.data, " Barcode type is " + e.type);
+  }
 
- handleTourch(value) {
-   if (value === true){
-     this.setState({ torchOn: false});
-   } else {
-     this.setState({ torchOn: true});
-   }
- }
+  handleTourch(value) {
+    if (value === true) {
+      this.setState({ torchOn: false });
+    } else {
+      this.setState({ torchOn: true });
+    }
+  }
 }
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     flexDirection: 'row'
-   },
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row'
+  },
 
-   preview: {
-     flex: 1,
-     justifyContent: 'flex-end',
-     alignItems: 'center'
-   },
-   cameraIcon: {
-     margin: 5,
-     height: 40,
-     width: 40
-   },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  cameraIcon: {
+    margin: 5,
+    height: 40,
+    width: 40
+  },
 
-   bottomOverlay: {
-     position: "absolute",
-     width: "100%",
-     flex: 20,
-     flexDirection: "row",
-     justifyContent: 'space-between'
-   },
- });
+  bottomOverlay: {
+    position: "absolute",
+    width: "100%",
+    flex: 20,
+    flexDirection: "row",
+    justifyContent: 'space-between'
+  },
+});
