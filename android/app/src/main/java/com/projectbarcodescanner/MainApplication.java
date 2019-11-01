792de333d68321ee65249181332e0c60cdff9073
Dan Boehmke <Daniel@webappclouds.com>
2019-11-01 11:35:24 -0400

    Imports

--- android/app/src/main/java/com/projectbarcodescanner/MainApplication.java
+++ android/app/src/main/java/com/projectbarcodescanner/MainApplication.java
@@ -4,11 +4,15 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+
+import org.pgsqlite.SQLitePluginPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -73,14 +77,15 @@ public class MainApplication extends Application implements ReactApplication {
     }
   }
 
-  /**
-   * A list of packages used by the app. If the app uses additional views
-   * or modules besides the default ones, add more packages here.
-   */
-  @Override
-  protected List<ReactPackage> getPackages() {
-    return Arrays.<ReactPackage>asList(
-      new SQLitePluginPackage(),   // register SQLite Plugin here 
-      new MainReactPackage());
-  }
+//  /**
+//   * A list of packages used by the app. If the app uses additional views
+//   * or modules besides the default ones, add more packages here.
+//   */
+//
+//  @Override
+//  protected List<ReactPackage> getPackages() {
+//    return Arrays.<ReactPackage>asList(
+//      new SQLitePluginPackage(),   // register SQLite Plugin here
+//      new MainReactPackage());
+//  }
 }
