deb574b1077e5d0dbf2e4c3aeb2533a814c8a89b
Dan Boehmke <Daniel@webappclouds.com>
2019-11-04 17:24:10 -0500

    Remove ProfileScreen

--- index.js
+++ index.js
@@ -4,11 +4,9 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import ProfileScreen from './ProfileScreen'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => ProfileScreen)
 
 var React = require('react-native');
 var SQLite = require('react-native-sqlite-storage')
