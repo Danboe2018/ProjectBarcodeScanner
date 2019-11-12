82097050d5ae95f6f4447aab6e55fc520d4f9419
Dan Boehmke <Daniel@webappclouds.com>
2019-11-12 14:08:53 -0500

    Updates to AsyncStorage

--- src/screens/AddToList.js
+++ src/screens/AddToList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
+import { AsyncStorage } from '@react-native-community/async-storage';
+
 class AddToList extends React.Component {
     constructor(props) {
         super(props);
@@ -17,16 +19,17 @@ class AddToList extends React.Component {
                 alignItems: 'flex-start',
                 justifyContent: 'center'
             }}>
-                <Text>Add to List:</Text>
+                <Text style={{ fontSize: 24, marginBottom: 10 }}>Add to List:</Text>
+
+                <Text>Product Name:</Text>
                 <TextInput
-                    style={{ height: 40, width: "95%", borderColor: 'gray', borderWidth: 1, marginBottom: 20 }}
-                    placeholder="Product Name:"
+                    style={{ height: 40, width: "95%", borderColor: 'black', borderWidth: 1, marginBottom: 20 }}
                     onChangeText={(text) => this.setState({ productName: text })}
                 />
 
+                <Text>Product Code:</Text>
                 <TextInput
-                    style={{ height: 40, width: "95%", borderColor: 'gray', borderWidth: 1, marginBottom: 20 }}
-                    placeholder="Product Code:"
+                    style={{ height: 40, width: "95%", borderColor: 'black', borderWidth: 1, marginBottom: 20 }}
                     value={JSON.stringify(navigation.getParam('productCode', "11"))}
                     onChangeText={(text) => this.setState({ productCode: text })}
                 />
@@ -34,7 +37,7 @@ class AddToList extends React.Component {
                 {barCodes.map((type) => <Text>{type.Product} {type.Code}</Text>)}
                 <View style={{
                     flex: 1,
-                    alignItems: 'center',
+                    alignSelf: 'center',
                     justifyContent: 'flex-end'
                 }}>
                     <Button title="Add To List" onPress={this.AddToArray} />
@@ -46,6 +49,28 @@ class AddToList extends React.Component {
     AddToArray = () => {
         barCodes.push({ Product: this.state.productName, Code: this.state.productCode })
         this.forceUpdate();
+        this._storeData();
+        this._retrieveData();
     }
+
+    _storeData = async () => {
+        try {
+            await AsyncStorage.setItem('TASKS', 'I like to save it.');
+        } catch (error) {
+            // Error saving data
+        }
+    };
+
+    _retrieveData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('TASKS');
+            if (value !== null) {
+                // We have data!!
+                console.log(value);
+            }
+        } catch (error) {
+            // Error retrieving data
+        }
+    };
 }
 export default AddToList;
